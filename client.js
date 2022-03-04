229a1489c9a161aa35580f786056ca445f2cceff
Michael Chuinard <mpchuinard@finlandia.edu>
2022-03-04 16:46:33 -0600

    started in js with newEmployee function

--- client.js
+++ client.js
@@ -3,3 +3,16 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('JQ is running');  
 }
+
+let employees = [];
+
+function newEmployee() {
+    console.log('in newEmployee:');
+    let newEmployeeObject = {
+        firstName: $('#firstName').val(), 
+        lastName: $('#lastName').val(),
+        id: $('#employeeId').val(),
+        title: $('#employeeTitle').val(),
+        annualSalary: $('#annualSalary').val()
+    }
+}
