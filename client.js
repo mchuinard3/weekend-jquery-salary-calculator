ccb4183e523911c6d2d3e29986be9cec7be778f7
Michael Chuinard <mpchuinard@finlandia.edu>
2022-03-04 19:06:25 -0600

    added second function

--- client.js
+++ client.js
@@ -1,16 +1,34 @@
 $(document).ready(readyNow);
 
+let monthlyTotal = 0;
+
 function readyNow() {
-    console.log('JQ is running');  
+    console.log('JQ is running');
+    $('#submitButton').on('click', addEmployee);
 }
 
-function newEmployee() {
+function newEmployee(firstName, lastName, employeeId, jobTitle, annualSalary) {
     console.log('in newEmployee:');
-    let newEmployeeObject = {
-        firstName: $('#firstName').val(), 
-        lastName: $('#lastName').val(),
-        id: $('#employeeId').val(),
-        title: $('#employeeTitle').val(),
-        annualSalary: $('#annualSalary').val()
+    
+   let newEmployeeObject = {
+        first: firstName,
+        last: lastName,
+        id: employeeId,
+        title: jobTitle,
+        salary: annualSalary
     }
 }
+   
+function addEmployee() {
+
+let firstName = $('.firstName').val();
+let lastName = $('.lastName').val();
+let employeeId = Number($('.employeeId').val());
+let jobTitle = $('.employeeTitle').val();
+let annualSalary = Number($('.annualSalary').val());
+
+$('#employeeInfo').append('<thead><tr><th>${firstName}</th><th>${lastName}</th><th>${employeeId}</th><th>${jobTitle}</th><th>${annualSalary}</th></thead>');
+newEmployee();
+}
+
+  
