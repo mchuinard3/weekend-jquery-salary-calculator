d633ad19b1498c8f2f4207be50da738315cec502
Michael Chuinard <mpchuinard@finlandia.edu>
2022-03-05 18:49:02 -0600

    began adding monthly total

--- client.js
+++ client.js
@@ -5,30 +5,48 @@ let monthlyTotal = 0;
 function readyNow() {
     console.log('JQ is running');
     $('#submitButton').on('click', addEmployee);
+    
 }
 
 function newEmployee(firstName, lastName, employeeId, jobTitle, annualSalary) {
     console.log('in newEmployee:');
-    
-   let newEmployeeObject = {
-        first: firstName,
-        last: lastName,
-        id: employeeId,
-        title: jobTitle,
-        salary: annualSalary
+
+    let newEmployeeObject = {
+    first: firstName,
+    last: lastName,
+    id: employeeId,
+    title: jobTitle,
+    salary: annualSalary
     }
+    return newEmployeeObject; 
 }
-   
+
 function addEmployee() {
+    
+    let firstName = $('.firstName').val();
+    let lastName = $('.lastName').val();
+    let employeeId = Number($('.employeeId').val());
+    let jobTitle = $('.employeeTitle').val();
+    let annualSalary = Number($('.annualSalary').val());
 
-let firstName = $('.firstName').val();
-let lastName = $('.lastName').val();
-let employeeId = Number($('.employeeId').val());
-let jobTitle = $('.employeeTitle').val();
-let annualSalary = Number($('.annualSalary').val());
+    $('#employeeInfo').append(`<tr><td>${firstName}</td><td>${lastName}</td><td>${employeeId}</td><td>${jobTitle}</td><td>${annualSalary}</td></tr>`);
+    $('.firstName').val('');
+    $('.lastName').val('');
+    $('.employeeId').val('');
+    $('.employeeTitle').val('');
+    $('.annualSalary').val('');
 
-$('#employeeInfo').append(`<thead><tr><th>${firstName}</th><th>${lastName}</th><th>${employeeId}</th><th>${jobTitle}</th><th>${annualSalary}</th></thead>`);
-newEmployee();
+    monthlyTotal += annualSalary / 12;
+    $('.monthlyCost').empty();
+    $('.monthlyCost').append(monthlyTotal);
+    newEmployee(firstName, lastName, employeeId, jobTitle, annualSalary);
+    // addMonthlyTotal();
 }
 
-  
+// function addMonthlyTotal() {
+    
+// }
+
+
+
+
